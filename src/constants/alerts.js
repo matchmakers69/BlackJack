c8e017cf4189b6d7786ad554c25b400d89763b39
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-06-19 22:41:21 +0100

    Added alerts as object to alerts.js

---
+++ src/constants/alerts.js
@@ -0,0 +1,8 @@
+export const alert = {
+  ONLY_NUMBERS: 'Your bet must be only a number!',
+  NO_FUNDS: 'Sorry pal, you do not have enough funds!',
+  PLAYER_WINS: 'You win! Your wife will love you!',
+  PLAYER_LOST: 'You lost! Your wife will kill you!',
+  DEALER_LOST: 'Dealer lost! Your wife will love you!',
+  DEALER_WINS: 'Dealer wins! Your wife will kill you!',
+};
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import { alert } from './alerts';
+
+export default {
+  ...alert,
+};
