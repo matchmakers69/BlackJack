0da8a5aaaf3577183539103fe781976900fb51a8
Przemyslaw <przemyslaw@Przemyslaws-iMac.local>
2020-06-19 22:57:36 +0100

    Added new alert message

--- src/constants/alerts.js
+++ src/constants/alerts.js
@@ -5,4 +5,5 @@ export const alert = {
   PLAYER_LOST: 'You lost! Your wife will kill you!',
   DEALER_LOST: 'Dealer lost! Your wife will love you!',
   DEALER_WINS: 'Dealer wins! Your wife will kill you!',
+  NO_ONE_WINS: 'Both wives happy!',
 };
